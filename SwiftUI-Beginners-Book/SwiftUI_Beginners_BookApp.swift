0ac4222ed09680d6db20e60ccff8debaeb96d100
Simran Preet Singh <simran5590@gmail.com>
2023-06-14 06:54:43 -0700

    Initial Commit

---
+++ SwiftUI-Beginners-Book/SwiftUI_Beginners_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Beginners_BookApp.swift
+//  SwiftUI-Beginners-Book
+//
+//  Created by Simran Preet Narang on 2023-06-14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Beginners_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
