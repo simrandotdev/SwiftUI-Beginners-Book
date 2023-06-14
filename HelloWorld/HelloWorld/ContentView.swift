95712ba81da9a6757ece0b8da6f2af3aebd30daf
Simran Preet Singh <simran5590@gmail.com>
2023-06-14 07:28:55 -0700

    Button
    A simple button with an action and a label

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -10,15 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Stay Hungry, Stay Foolish")
-//                .fontWeight(.bold)
-//                .fontWeight(.heavy)
-                .fontWeight(.black)
-                .font(.largeTitle)
-                .fontDesign(.rounded)
+            Button(action: {}, label: {
+                Text("Tap me!")
+                    .font(.system(.title, design: .rounded, weight: .heavy))
+            })
         }
         .padding()
     }
