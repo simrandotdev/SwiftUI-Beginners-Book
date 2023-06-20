f2e54ebaaf6588f30938a8f99c36468ec7da0cfd
Simran Preet Singh <simran5590@gmail.com>
2023-06-20 13:42:30 -0700

    Using a VStack to show 2 buttons

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -17,7 +17,6 @@ struct ContentView: View {
             Button(action: {
                 let utterance = AVSpeechUtterance(string: "Hello World")
                 utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
-                
                 synthesizer.speak(utterance)
             }, label: {
                 Text("Hello World")
@@ -27,11 +26,92 @@ struct ContentView: View {
             .foregroundStyle(Color.white)
             .background(Color.blue)
             .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            Button(action: {
+                let utterance = AVSpeechUtterance(string: "Happy Programming")
+                utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+                synthesizer.speak(utterance)
+            }, label: {
+                Text("Happy Programming")
+                    .font(.system(.title, design: .rounded, weight: .heavy))
+            })
+            .padding()
+            .foregroundStyle(Color.white)
+            .background(Color.yellow)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+extension View {
+    func navigationStack(withTitle title: String) -> some View {
+        return NavigationStack {
+            self
+                .navigationTitle(title)
+        }
+    }
+    
+    
+    func navigationStack(withTitle title: String, path: Binding<NavigationPath>) -> some View {
+        return NavigationStack(path: path) {
+            self
+                .navigationTitle(title)
+        }
+    }
+    
+    func embedInZStack() -> some View {
+        return ZStack {
+            self
+        }
+    }
+    
+    func embedInHStack() -> some View {
+        return HStack {
+            self
+        }
+    }
+    
+    func embedInVStack() -> some View {
+        return VStack {
+            self
+        }
+    }
+    
+    func embedIn(stack: Stack) -> some View {
+        switch stack {
+        case .HStack:
+            return AnyView(embedInHStack())
+        case .VStack:
+            return AnyView(embedInVStack())
+        case .ZStack:
+            return AnyView(embedInZStack())
+        }
+    }
+    
+    func backgroundColor(_ color: Color) -> some View {
+        return ZStack {
+            color
+            self
+        }
+    }
+    
+    func navigationLink(destination: () -> some View) -> some View {
+        return NavigationLink(destination: destination) {
+            self
+        }
+    }
+}
+
+enum Stack {
+    case HStack
+    case VStack
+    case ZStack
 }
