e133b5730bd45ed3037e366903ad0f9916fce8b4
Simran Preet Singh <simran5590@gmail.com>
2023-06-20 10:53:51 -0700

    Performing an action on click of a button

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    
+    let synthesizer = AVSpeechSynthesizer()
+    
     var body: some View {
         VStack {
-            Button(action: {}, label: {
-                Text("Tap me!")
+            Button(action: {
+                let utterance = AVSpeechUtterance(string: "Hello World")
+                utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+                
+                synthesizer.speak(utterance)
+            }, label: {
+                Text("Hello World")
                     .font(.system(.title, design: .rounded, weight: .heavy))
             })
             .padding()
