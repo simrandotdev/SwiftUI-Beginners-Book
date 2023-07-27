fd54a3048a21cd7a6e078efa74d0d222a7534406
Simran Preet Singh <simran5590@gmail.com>
2023-07-26 20:54:54 -0700

    removed individual arrays

--- FoodPin/FoodPin/Views/RestaurantListView.swift
+++ FoodPin/FoodPin/Views/RestaurantListView.swift
@@ -8,14 +8,6 @@
 import SwiftUI
 
 struct RestaurantListView: View {
-    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
-    
-    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
-    
-    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney",
-    "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
-    
-    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian/ Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee &Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
     
     @State var restaurantIsFavorites = Array(repeating: false, count: 21)
     @State var restaurants = [
@@ -102,11 +94,8 @@ struct BasicTextImageView: View {
             
             Spacer()
             
-            if restaurant.isFavorite
-            {
-                Image(systemName: "heart.fill")
-                    .foregroundColor(.red)
-            }
+            Image(systemName: restaurant.isFavorite ? "heart.fill" : "heart")
+                .foregroundColor(.red)
         }
         .contentShape(Rectangle())
         .onTapGesture
@@ -171,11 +160,8 @@ struct FullImageRow: View {
                 
                 Spacer()
                 
-                if restaurant.isFavorite
-                {
-                    Image(systemName: "heart.fill")
-                        .foregroundColor(.red)
-                }
+                Image(systemName: restaurant.isFavorite ? "heart.fill" : "heart")
+                    .foregroundColor(.red)
             }
             
         }
