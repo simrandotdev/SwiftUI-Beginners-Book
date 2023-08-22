1b953093b2fdfcb49996fd09acd339c641606e8c
Simran Preet Singh <simran5590@gmail.com>
2023-08-21 17:00:26 -0700

    Applying a Visual Blur Effect to the background of the `ReviewView`

--- FoodPin/FoodPin/Views/ReviewView.swift
+++ FoodPin/FoodPin/Views/ReviewView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ReviewView: View {
+    
+    var restaurant: Restaurant
+    
     var body: some View {
         ZStack {
-            Color.black.ignoresSafeArea()
+            
+            Image(restaurant.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .ignoresSafeArea()
+            
+            Color.black
+                .opacity(0.6)
+                .background(.ultraThinMaterial)
+                .ignoresSafeArea()
             
             HStack {
                 Spacer()
@@ -46,6 +59,12 @@ struct ReviewView: View {
 
 struct ReviewView_Previews: PreviewProvider {
     static var previews: some View {
-        ReviewView()
+        ReviewView(restaurant: Restaurant(name: "Cafe Deadend",
+                                          type: "Coffee & Tea Shop",
+                                          location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong",
+                                          phone: "232-923423",
+                                          description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.",
+                                          image: "cafedeadend",
+                                          isFavorite: true))
     }
 }
